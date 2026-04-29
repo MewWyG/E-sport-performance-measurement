dda36f23db70448172abb2e9bc35249fcbbeb30e
MewWyG <apiwit22guy@gmail.com>
2026-04-29 21:36:19 +0700

    feat: add game library and moving target flow

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './App'
-import './App.css'
+import { LibraryGamePage } from './Pages/librarygame/LibraryGamePage'
+import { MovingTargetInfoPage } from './Pages/gameinfo/movingtarget_info/movingTarget_info'
+import MovingTargetGamePage from './Pages/gamepages/movingtarget/movingTarget'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/librarygame" element={<LibraryGamePage />} />
+        <Route path="/gameinfo/movingtarget" element={<MovingTargetInfoPage />} />
+        <Route path="/gameplay/movingtarget" element={<MovingTargetGamePage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
